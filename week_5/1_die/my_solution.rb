11ba2eac1abfd1d3eb69943e5074e2f95818db82
Amelia <downs.amelia@gmail.com>
2014-04-28 20:28:03 -0500

    did challenges 1 and 2

--- week_5/1_die/my_solution.rb
+++ week_5/1_die/my_solution.rb
@@ -5,30 +5,58 @@
 
 # 2. Pseudocode
 
-# Input:
-# Output:
-# Steps:
+# Input: 
+# number of sides of the die
+
+# Output: 
+# .sides should output the correct nuber of sides 
+# .roll should return a random number between 1 and the number of 
+# sides (includive)
+
+# Steps: 
+# create a die 
+# save the number of sides 
+# for .sides, return this saved number
+# create an exception for decimal numbers
+# create a random number generator 
+# for .roll, call the random number generator 
 
 
 # 3. Initial Solution
 
-class Die
-  def initialize(sides)
-    # code goes here
-  end
+# class Die
+#   def initialize(sides)
+#     raise ArgumentError if (sides < 1)
+#     @sides = sides;
+#   end
   
-  def sides
-    # code goes here
-  end
+#   def sides
+#     return @sides
+#   end
   
-  def roll
-    # code goes here
-  end
-end
+#   def roll
+#     1 + rand(@sides)
+#   end
+# end
 
 
 
 # 4. Refactored Solution
+
+class Die
+  def initialize(sides)
+    raise ArgumentError.new("Error") if ((sides < 1) || ((sides % 1) != 0))
+    @sides = sides;
+  end
+  
+  def sides
+    return @sides
+  end
+  
+  def roll
+    1 + rand(@sides)
+  end
+end
 
 
 
@@ -38,8 +66,34 @@ end
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
 
+#if sides <1, ArgumentError should be raised
+
+Die.new(0) rescue 
+puts $!.message == "Error"
+
+#if sides is not an intiger, ArgumentError should be raised
+
+Die.new(1.6) rescue 
+puts $!.message == "Error"
+
+#die should return correct number of sides
+puts Die.new(2).sides == 2
 
 
 
 
 # 5. Reflection 
+
+
+# I studied C++ in school so the concept of classes is not a new one to me. 
+# So this was more of a ruby sytax review for me in terms of classes. However
+# what I really learned was how to check for an argument error. That part of the
+# driver tests is the part of this challenge that took me the longest.
+
+# Now that I know how, I'm sure I'll be using it a lot from now on. My first 
+# attempt was to copy and paste from the 1_die_spec.rb, with hopes that I could 
+# edit parts to test for the things I wanted, but I couldn't get it to work. 
+# I really wish I understood the rspec code more. 
+
+# I am happy to be delving more into Ruby. 
+
--- week_5/2_die/my_solution.rb
+++ week_5/2_die/my_solution.rb
@@ -5,38 +5,108 @@
 
 # 2. Pseudocode
 
-# Input:
-# Output:
+# Input: 
+# an array
+
+# Output: 
+# .sides returns the number of element in the labels array 
+# (thus, number of sides)
+# .roll returns a random element from the array
+
 # Steps:
+# determine the length of the array 
+# find a random number (n) between 1 and the length of the array
+# return the nth element in the array
 
 
 # 3. Initial Solution
 
-class Die
-  def initialize(labels)
-  end
+# class Die
+#   def initialize(labels)
+#   	@length = labels.length
+#   	raise ArgumentError.new("Error") if (@length == 0)
+#     @labels = labels;
+#   end
 
-  def sides
-  end
+#   def sides
+#   	return @length
+#   end
 
-  def roll
-  end
-end
+#   def roll
+#   	return @labels[rand(@length)]
+#   end
+# end
 
 
 
 # 4. Refactored Solution
 
+class Die
+  def initialize(labels)
+  	@sides = labels.length
+  	raise ArgumentError.new("Error") if (@sides == 0)
+    @labels = labels;
+  end
 
+  def sides
+  	return @sides
+  end
+
+  def roll
+  	return @labels[rand(@sides)]
+  end
+end
 
 
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
+# raises argument error if list is empty 
+Die.new([]) rescue 
+puts $!.message == "Error"
 
+# die#sides returns number of sides given at initiation
 
+puts Die.new([1,2,3,4,5]).sides == 5
 
+# roll returns every possible letter for sufficiently high # of rolls
+# This array will represent all of the sides of the die
+bool_array = Array.new(100, false)
+
+# Created a die with the numbers 1-99 on it
+test_array = (0..99).to_a
+test_die = Die.new(test_array)
+
+# roll the die 1000 times
+# each time a number is rolled
+# that spot on bool_array is marked true
+1000.times{
+	bool_array[test_die.roll] = true
+}
+
+# go through bool_array and make sure all are marked true
+test_result = true
+bool_array.each do |x|
+	if x == false
+		test_result = false
+		break
+	end 
+end
+
+puts test_result
 
 
 # 5. Reflection 
+
+# I like that this challenge built on the last one and I like this concept. 
+# Phase 0 challenges have done a good job of using fun challenges that I 
+# can see the actaul value in rather than just straight teaching code 
+
+# I really like that you don't teach us methods, you just give us problems
+# and if I need help I look it up, just like I would in the real world. Doing 
+# it this way makes me feel very confident in my self.
+
+# What I like most about the more recent challenges is learning how to write 
+# test code. There are so many things to test for! Also these past 2 challenges
+# have introduced me to ArgumentError which I had never heard of before. 
